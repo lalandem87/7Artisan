d491e1b0af3be48558399d9500dda7c689979807
Dixxh <lalandemaxime37@gmail.com>
2026-05-29 15:48:11 +0200

    Add Barre Contact to home page

---
+++ src/components/Barre-Contact/Barre-Contact.jsx
@@ -0,0 +1,22 @@
+import { Phone } from "lucide-react";
+import "./Barre-Contact.scss";
+import { Link } from "react-router-dom";
+
+export function BarreContact({ title, sentence }) {
+  return (
+    <div className="barre-contact">
+      <div className="left">
+        <h2>{title}</h2>
+        <p>{sentence}</p>
+      </div>
+      <div className="right">
+        <a href="" className="btn-appl">
+          <Phone size={15} /> Appeler maintenant
+        </a>
+        <Link to="/contact" className="btn-form">
+          Formulaire de contact
+        </Link>
+      </div>
+    </div>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import { HeroHome } from "../components/Hero-Home/Hero-Home";
 import { BarreInfo } from "../components/Barre-Info/Barre-Info";
 import { Expertise } from "../components/Expertise/Expertise";
 import { Avis } from "../components/Avis/Avis";
+import { BarreContact } from "../components/Barre-Contact/Barre-Contact";
 import data from "../../backend/data.json";
 
 const dataAvis = data["avis"].slice(0, 3);
@@ -20,6 +21,10 @@ export function Home() {
         nos
         clients"
         data={dataAvis}
+      />
+      <BarreContact
+        title="Prêt à transformer votre maison ?"
+        sentence="Devis gratuit, réponse sous 48h, sans engagement."
       />
     </>
   );

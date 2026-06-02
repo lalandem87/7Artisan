b470a245db94214cfe1d60a818042c588dcc4c20
Dixxh <lalandemaxime37@gmail.com>
2026-06-02 15:16:42 +0200

    Add section services to service page

---
+++ src/components/Show-Services/Show-Services.jsx
@@ -0,0 +1,58 @@
+import "./Show-Services.scss";
+import data from "../../../backend/data.json";
+import { Link } from "react-router-dom";
+import { ArrowRight, Check } from "lucide-react";
+
+const services = data["services"];
+
+export function ShowServices() {
+  return (
+    <section className="services">
+      <div className="container-card">
+        {services.map((service) => {
+          return (
+            <div
+              className={`card-service ${service.id % 2 !== 0 ? "reverse" : ""}`}
+              key={service.id}
+            >
+              <div className="left">
+                <span
+                  className="badge-card"
+                  style={{
+                    color: service.badge.color,
+                    background: service.badge.backcolor,
+                  }}
+                >
+                  {service.badge.name}
+                </span>
+                <h2 className="title">{service.name}</h2>
+                <p className="desc">{service.desc}</p>
+                <ul>
+                  {service.lists.list.map((item, id) => {
+                    return (
+                      <li
+                        style={{ background: service.lists.backcolor }}
+                        key={id}
+                      >
+                        <Check />
+                        {item}
+                      </li>
+                    );
+                  })}
+                </ul>
+                <Link to="contact">
+                  En savoir plus <ArrowRight />
+                </Link>
+              </div>
+              <div className="right">
+                <div className="img-container">
+                  <img src={service["image-url"]} alt={service.name} />
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
+}
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,5 +1,11 @@
 import { HeroServices } from "../components/Hero-Service/Hero-Service";
+import { ShowServices } from "../components/Show-Services/Show-Services";
 
 export function Services() {
-  return <HeroServices />;
+  return (
+    <>
+      <HeroServices />
+      <ShowServices />
+    </>
+  );
 }

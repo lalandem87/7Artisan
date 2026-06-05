74d0c52b18c76be69dc91f7f135805fd88e78c11
Dixxh <lalandemaxime37@gmail.com>
2026-06-05 18:28:39 +0200

    Button categories works and Add Portfolio section with all pictures

--- src/components/Filtre-Categories/Categories.jsx
+++ src/components/Filtre-Categories/Categories.jsx
@@ -2,14 +2,23 @@ import data from "../../../backend/data.json";
 import "./Categories.scss";
 
 const allCategories = data["categories"];
+const works = data["works"];
 
-export function DisplayCategories() {
+export function DisplayCategories({ work, onFilter }) {
   return (
     <div className="categories">
-      <button className="categorie active">Tous</button>
+      <button className="categorie active" onClick={() => onFilter(works)}>
+        Tous
+      </button>
       {allCategories.map((cate) => {
         return (
-          <button key={cate.id} className="categorie">
+          <button
+            key={cate.id}
+            className="categorie"
+            onClick={() =>
+              onFilter(works.filter((work) => work.categorie_id === cate.id))
+            }
+          >
             {cate.name}
           </button>
         );
--- src/pages/Realisations.jsx
+++ src/pages/Realisations.jsx
@@ -1,8 +1,14 @@
 import { HeroReaCont } from "../components/Hero-Rea-Cont/Hero-Rea-Cont";
 import { BarreContact } from "../components/Barre-Contact/Barre-Contact";
 import { DisplayCategories } from "../components/Filtre-Categories/Categories.jsx";
+import { Portfolio } from "../components/Portfolio/Portfolio.jsx";
+import { useState } from "react";
+import data from "../../backend/data.json";
+
+const works = data["works"];
 
 export function Realisations() {
+  const [filtered, setFiltered] = useState(works);
   return (
     <>
       <HeroReaCont
@@ -11,7 +17,8 @@ export function Realisations() {
         desc="Découvrez une sélection de nos chantiers récents — chaque projet est
       unique et réalisé avec le même niveau d'exigence."
       />
-      <DisplayCategories />
+      <DisplayCategories work={works} onFilter={setFiltered} />
+      <Portfolio workToDisplay={filtered} />
       <BarreContact
         title="Votre projet sera notre prochaine réalisation."
         sentence="Contactez-nous pour un échange gratuit et sans engagement."
